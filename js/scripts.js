d8b514626797e73b9d421c838cb5a39a9e88f3e6
Arun Kumar <arunkumarm1995@gmail.com>
2016-12-09 21:13:29 +0530

    Implement smooth scroll

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,22 @@ $(document).ready(function () {
     full_width: true,
     indicators: false
   });
+
+  // Smooth scroll
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    // Check if the link target belongs this to page
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+      location.hostname === this.hostname) {
+        // hash property returns the anchor part of the url
+        // The following line removes the pound sign from the target and finds
+        // the target element
+        // For more, read http://lea.verou.me/2011/05/get-your-hash-the-bulletproof-way/
+        target = $('[id=' + this.hash.slice(1) + ']');
+
+        if (target.length)
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+    }
+  })
 });
